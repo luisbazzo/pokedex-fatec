57c7b3c09f644d54cf02a529c12f3a7dbf5973a9
luisbazzo <luis.o.c.bazzo@gmail.com>
2023-04-25 10:47:33 -0300

    Segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PokedexComponent } from './pokedex/pokedex.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { IonicModule } from '@ionic/angular';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    IonicModule.forRoot(),
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -6,5 +6,87 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokedex.component.css']
 })
 export class PokedexComponent {
+  poke_number : number = 1;
+  poke_number_str : string = "001";
+  poke_name : string = "Bulbasaur";
 
+  poke_str_prev : string = "1008";
+  poke_str_next : string = "002";
+
+  poke_name_prev : string = "Miraidon";
+  poke_name_next : string = "Ivysaur";
+
+  poke_img : string = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + this.poke_number_str + ".png";
+
+  pokeNumberToString(){
+    if(this.poke_number >= 10 && this.poke_number < 100){
+      this.poke_number_str = "0" + this.poke_number;
+    }
+    else if(this.poke_number >= 100){
+      this.poke_number_str = this.poke_number.toString();
+    }
+    else{
+      this.poke_number_str = "00" + this.poke_number;
+    }
+
+    if(parseInt(this.poke_str_next) >= 10 && parseInt(this.poke_str_next) < 100){
+      this.poke_str_next = "0" + this.poke_str_next;
+    }
+    else if(parseInt(this.poke_str_next) < 10){
+      this.poke_str_next = "00" + this.poke_str_next;
+    }
+
+    if(parseInt(this.poke_str_prev) >= 10 && parseInt(this.poke_str_prev) < 100){
+      this.poke_str_prev = "0" + this.poke_str_prev;
+    }
+    else if(parseInt(this.poke_str_prev) < 10){
+      this.poke_str_prev = "00" + this.poke_str_prev;
+    }
+  }
+
+  nextPoke(){
+    let reset : number = 0;
+    this.poke_number = this.poke_number + 1;
+    if(this.poke_number >= 1009){
+      this.poke_number = 1;
+      reset = 1;
+    }
+
+    this.poke_str_next = (this.poke_number + 1).toString();
+    this.poke_str_prev = (this.poke_number - 1).toString();
+
+    if(reset == 1){
+      this.poke_str_prev = "1008";
+    }
+
+    if(parseInt(this.poke_str_next) >= 1009){
+      this.poke_str_next = "1";
+    }
+
+    this.pokeNumberToString();
+    this.poke_img = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + this.poke_number_str + ".png";
+  }
+
+  previousPoke(){
+    let reset : number = 0;
+    this.poke_number = this.poke_number - 1;
+    if(this.poke_number <= 0){
+      this.poke_number = 1008;
+      reset = 1;
+    }
+
+    this.poke_str_next = (this.poke_number + 1).toString();
+    this.poke_str_prev = (this.poke_number - 1).toString();
+
+    if(reset == 1){
+      this.poke_str_next = "1";
+    }
+
+    if(parseInt(this.poke_str_prev) <= 0){
+      this.poke_str_prev = "1008";
+    }
+
+    this.pokeNumberToString();
+    this.poke_img = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + this.poke_number_str + ".png";
+  }
 }
